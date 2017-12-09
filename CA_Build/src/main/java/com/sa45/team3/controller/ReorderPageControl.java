2f693ecdb16bb44c6877b7767d21f72b9594c8c5
Michael Thong <thong.michael@gmail.com>
2017-12-10 00:57:59 +0800

    ..............

--- CA_Build/src/main/java/com/sa45/team3/controller/ReorderPageControl.java
+++ CA_Build/src/main/java/com/sa45/team3/controller/ReorderPageControl.java
@@ -1,5 +1,16 @@
 package com.sa45.team3.controller;
 
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.sa45.team3.repository.ProductRepository;
+
+@RequestMapping(value="/admin")
+@Controller
 public class ReorderPageControl {
 
+	@Resource
+	ProductRepository prepo;
 }
