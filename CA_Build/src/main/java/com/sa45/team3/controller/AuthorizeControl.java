226f692a2cd098dd662325eba303817316f96421
Boyka <boyka59452@gmail.com>
2017-12-10 11:09:16 +0800

    refine the Authorize

--- CA_Build/src/main/java/com/sa45/team3/controller/AuthorizeControl.java
+++ CA_Build/src/main/java/com/sa45/team3/controller/AuthorizeControl.java
@@ -41,9 +41,9 @@ public class AuthorizeControl{
 	
 	@RequestMapping(value="/validate", method= RequestMethod.POST)
 	public ModelAndView validation(@ModelAttribute Staff staff, HttpSession session, BindingResult result) {
-		
-		ModelAndView mav = new ModelAndView("product-list"); //create jsp
 		session.setAttribute("role", "admin");
+		ModelAndView mav = new ModelAndView("product-list"); //create jsp
+		
 		return mav;
 		
 	}
