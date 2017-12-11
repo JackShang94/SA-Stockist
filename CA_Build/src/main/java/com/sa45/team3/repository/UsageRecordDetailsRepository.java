b6e385141d8bb8be6778540d2dfe106ef8b4c899
WesLeeWH <33190336+WesLeeWH@users.noreply.github.com>
2017-12-11 18:08:03 +0800

    updated usage details repo

--- CA_Build/src/main/java/com/sa45/team3/repository/UsageRecordDetailsRepository.java
+++ CA_Build/src/main/java/com/sa45/team3/repository/UsageRecordDetailsRepository.java
@@ -6,13 +6,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-
+import com.sa45.team3.model.UsageDetailsPrimaryKey;
 import com.sa45.team3.model.UsageRecordDetails;
 
-public interface UsageRecordDetailsRepository extends JpaRepository<UsageRecordDetails, Integer> {
+public interface UsageRecordDetailsRepository extends JpaRepository<UsageRecordDetails, UsageDetailsPrimaryKey> {
 
 
-	@Query("SELECT d from UsageRecordDetails d WHERE d.recordID = :rid")
-	ArrayList<UsageRecordDetails> findUsageDetailsByrID(@Param("rid") int rid);
+	@Query("SELECT d from UsageRecordDetails d WHERE d.primarykey.recordID =:rid")
+	ArrayList<UsageRecordDetails> findUsageDetailsByrID(@Param("rid") Integer rid);
 
 }
